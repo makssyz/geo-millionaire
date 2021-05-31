415512a0f442b6d727b2428e0b60ae9f016e2794
Makss Golubs <makss.golubs@ibm.com>
2021-05-31 12:05:44 +0200

    Initial commit

---
+++ GeoMillionaire/GeoMillionaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoMillionaireApp.swift
+//  GeoMillionaire
+//
+//  Created by Makss Golubs on 31.05.21.
+//
+
+import SwiftUI
+
+@main
+struct GeoMillionaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
